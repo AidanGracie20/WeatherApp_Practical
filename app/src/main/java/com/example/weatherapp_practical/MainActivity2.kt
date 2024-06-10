63f591018530c711657d8a326e545a47f6d4dc3b
AidanGracie20 <ggracie0120@gmail.com>
2024-06-10 10:17:27 +0200

    push after designing

--- app/src/main/java/com/example/weatherapp_practical/MainActivity2.kt
+++ app/src/main/java/com/example/weatherapp_practical/MainActivity2.kt
@@ -1,11 +1,20 @@
 package com.example.weatherapp_practical
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity2 : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main2)
+
+        var buttonMo = findViewById<Button>(R.id.buttonMo)
+        buttonMo.setOnClickListener {
+            val intent = Intent(this, MainActivity3::class.java)
+            startActivity(intent) }
+
+
     }
 }
